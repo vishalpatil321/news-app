8b95abdae3f08b0ad43e01c8f2ee048f2da95b43
vishalpatil321 <vishalchhagan28@gmail.com>
2024-08-07 21:09:17 +0530

    news

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotEnv = require('dotenv');
+const morgan = require('morgan');
+const connectDb = require('./dbConnection');
+const router = require('./routes');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors());
+
+dotEnv.config();
+app.use(express.json());
+app.use(morgan('dev'));
+
+//databse connection
+connectDb();
+
+app.use('/api',router);
+app.use(express.static(path.join(__dirname,"../client/build")));
+
+app.get('*',(req,res) => {
+     res.sendFile(path.join(__dirname,"../client/build/index.html"));
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT , () => {
+     console.log('server running..');
+})
