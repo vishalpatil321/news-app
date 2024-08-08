cc3ed81193369cd099d356aba85943f989abe918
Vishal Dhone <150796650+vishalpatil321@users.noreply.github.com>
2024-08-08 15:08:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  "origin": "http://localhost:3000",
+  "origin": "*",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204
